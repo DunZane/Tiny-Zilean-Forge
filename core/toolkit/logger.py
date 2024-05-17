9bacb54b9dfd44f4656643f3fa0551d4842fbf7b
“HumbleSwage” <d_zhao_work@163.com>
2024-05-17 09:25:59 +0800

    Update Comments

--- core/toolkit/logger.py
+++ core/toolkit/logger.py
@@ -1,25 +1,23 @@
 import logging
 import datetime
 
-
-def setup_logger(logger_name,log_level=logging.INFO):
+def setup_logger(logger_name, log_level=logging.INFO):
     logger = logging.getLogger(logger_name)
     logger.setLevel(log_level)
-    # 创建一个格式化程序
+    # Create a formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    # 创建一个文件处理程序
-    log_file_path = "logs/{}_{}.log".format(logger_name,datetime.datetime.now().strftime("%Y-%m-%d_%H"))
-    # 用于输出至文件
+    # Create a file handler
+    log_file_path = "logs/{}_{}.log".format(logger_name, datetime.datetime.now().strftime("%Y-%m-%d_%H"))
+    # For output to file
     file_handler = logging.FileHandler(log_file_path)
     file_handler.setLevel(log_level)
-    # 将格式化程序添加到文件处理程序
+    # Add the formatter to the file handler
     file_handler.setFormatter(formatter)
-    # 将文件处理程序添加到日志记录器
+    # Add the file handler to the logger
     logger.addHandler(file_handler)
-    # 创建一个控制台处理程序（可选，用于同时输出到控制台）
+    # Create a console handler (optional, for simultaneous output to console)
     ch = logging.StreamHandler()
     ch.setLevel(log_level)
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
-
--- core/toolkit/tools.py
+++ core/toolkit/tools.py
@@ -5,17 +5,17 @@ import resource
 
 def get_current_memory_utilization():
     memory = psutil.virtual_memory()
-    # 返回已使用的内存，以MB为单位
+    # Return used memory in MB
     return memory.used / (1024 * 1024)
 
 
 def get_current_cpu_utilization():
-    # 返回cpu的使用率
+    # Return CPU utilization
     return psutil.cpu_percent(interval=1)
 
 
 def get_current_cpu_available():
-    # 返回cpu的可用率
+    # Return CPU availability
     return 100 - psutil.cpu_percent(interval=1)
 
 
@@ -35,7 +35,7 @@ def get_timeout_from_cmd(cmd):
 
 
 def get_current_mem_percent():
-    # 获取内存使用情况
+    # Get memory usage
     mem = psutil.virtual_memory()
     return mem.percent
 
@@ -43,7 +43,7 @@ def get_current_mem_percent():
 def get_available_memory():
     memory = psutil.virtual_memory()
     bytes_val = memory.available
-    # 最后返回的形式是以M的形式返回
+    # Return available memory in MB
     return bytes_val / (1024 * 1024)
 
 
@@ -52,24 +52,24 @@ def get_current_processes_num():
         all_processes = list(psutil.process_iter())
         return len(all_processes)
     except FileNotFoundError as e:
-        # 处理FileNotFoundError异常
+        # Handle FileNotFoundError exception
         print(f"An error occurred: {e}. Check if the required files are accessible.")
         return 20
     except Exception as e:
-        print(f"unexpect error occurred: {e}.")
+        print(f"Unexpected error occurred: {e}.")
         return 20
 
 
 # def get_available_processes_num():
-#     # 运行 ulimit -u 命令并捕获输出
+#     # Run ulimit -u command and capture output
 #     process = subprocess.Popen(['ulimit', '-u'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #     output, error = process.communicate()
 #
-#     # 解析输出，获取进程数量
+#     # Parse output to get the number of processes
 #     if output:
-#         output = output.strip().decode()  # 将字节转换为字符串
+#         output = output.strip().decode()  # Convert bytes to string
 #         if output == 'unlimited':
-#             max_processes = float('inf')  # 表示无限大
+#             max_processes = float('inf')  # Represent infinity
 #         else:
 #             max_processes = int(output)
 #             return max_processes - get_current_processes_num()
@@ -78,14 +78,14 @@ def get_current_processes_num():
 
 
 def get_available_processes_num():
-    # 获取进程数量限制
+    # Get process limit
     soft, hard = resource.getrlimit(resource.RLIMIT_NPROC)
     if soft == resource.RLIM_INFINITY:
-        max_processes = float('inf')  # 表示无限大
+        max_processes = float('inf')  # Represent infinity
     else:
         max_processes = soft
     # Number of fork workers must be between 0 and 4096
-    return 4096 - get_current_processes_num()  # 你需要实现 get_current_processes_num() 函数
+    return 4096 - get_current_processes_num()  # You need to implement get_current_processes_num() function
 
 
 if __name__ == '__main__':
